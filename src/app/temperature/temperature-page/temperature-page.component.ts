1cedc73c5d96b259d9469613252812fbd7a2cd6c
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2021-03-25 15:33:45 +0200

    GeneratedDataService 1

---
+++ src/app/temperature/temperature-page/temperature-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-page',
+  templateUrl: './temperature-page.component.html',
+  styleUrls: ['./temperature-page.component.less']
+})
+export class TemperaturePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/temperature/temperature.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemperaturePageComponent } from './temperature-page/temperature-page.component';
+
+
+
+@NgModule({
+  declarations: [TemperaturePageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemperatureModule { }
