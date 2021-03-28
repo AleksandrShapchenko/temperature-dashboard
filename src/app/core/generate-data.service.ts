4691ccd8d6fdb05135f48ad172cde3d84d37e1a2
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2021-03-28 18:04:10 +0300

    added events time checking

--- src/app/core/generate-data.service.ts
+++ src/app/core/generate-data.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GenerateDataService {
 
   constructor() {
   }
+
   temperature = 9;
   humidity = 40;
   airPressure = 10;
@@ -17,32 +18,30 @@ export class GenerateDataService {
     this.temperature = Math.random() < 0.5
       ? this.temperature += Math.round(Math.random())
       : this.temperature -= Math.round(Math.random());
-    console.log('temperature: ', this.temperature);
     document.dispatchEvent(this.generateTemperatureEvent);
-  }
+  };
   private generateHumidity = () => {
     this.humidity = Math.random() < 0.5
       ? this.humidity += Math.round(Math.random())
       : this.humidity -= Math.round(Math.random());
-    console.log('humidity: ', this.humidity);
     document.dispatchEvent(this.generateHumidityEvent);
-  }
+  };
   private generateAirPressure = () => {
     this.airPressure = Math.random() < 0.5
       ? this.airPressure += Math.round(Math.random())
       : this.airPressure -= Math.round(Math.random());
-    console.log('airPressure: ', this.airPressure);
     document.dispatchEvent(this.generateAirPressureEvent);
-  }
+  };
 
   planAhead(callback: () => void): void {
     setTimeout(() => {
       callback();
       this.planAhead(callback);
-      }, 100 + Math.random() * 1900);
+    }, 100 + Math.random() * 1900);
   }
 
   temperatureStream = this.planAhead(this.generateTemperature);
   humidityStream = this.planAhead(this.generateHumidity);
   airPressureStream = this.planAhead(this.generateAirPressure);
+
 }
