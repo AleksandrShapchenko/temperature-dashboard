c3a694451ab332cf668a3ec8befefd82501d4a42
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2021-03-29 12:19:33 +0300

    added canceller function into planAhead return

--- src/app/core/generate-data.service.ts
+++ src/app/core/generate-data.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {Canceller} from '../shared/models/canceller';
+import set = Reflect.set;
 
 @Injectable()
 export class GenerateDataService {
@@ -19,26 +21,35 @@ export class GenerateDataService {
       ? this.temperature += Math.round(Math.random())
       : this.temperature -= Math.round(Math.random());
     document.dispatchEvent(this.generateTemperatureEvent);
-  };
+  }
   private generateHumidity = () => {
     this.humidity = Math.random() < 0.5
       ? this.humidity += Math.round(Math.random())
       : this.humidity -= Math.round(Math.random());
     document.dispatchEvent(this.generateHumidityEvent);
-  };
+  }
   private generateAirPressure = () => {
     this.airPressure = Math.random() < 0.5
       ? this.airPressure += Math.round(Math.random())
       : this.airPressure -= Math.round(Math.random());
     document.dispatchEvent(this.generateAirPressureEvent);
-  };
+  }
 
-  planAhead(callback: () => void): void {
+  planAhead(callback: () => void): Canceller {
+    let work = true;
     setTimeout(() => {
       callback();
-      this.planAhead(callback);
+      if (work) {
+        this.planAhead(callback);
+      }
     }, 100 + Math.random() * 1900);
+
+    return function canceller(): void {
+      work = false;
+    };
   }
+
+
 
   temperatureStream = this.planAhead(this.generateTemperature);
   humidityStream = this.planAhead(this.generateHumidity);
