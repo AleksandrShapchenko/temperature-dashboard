2e1a02027673885acc1a5feda98206e3944a05fd
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2021-04-01 15:34:24 +0300

    refactored => subscribtion replaced by async pipe, merge replaced by combineLatest & added debounceTime

--- src/app/shared/models/display-object.ts
+++
@@ -1,5 +0,0 @@
-export interface DisplayObject {
-  airPressure: number | string | undefined;
-  humidity: number | string | undefined;
-  temperature: number | string | undefined;
-}
--- src/app/temperature/temperature/temperature.component.ts
+++ src/app/temperature/temperature/temperature.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent, merge, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { combineLatest, fromEvent, Observable } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
-import { DisplayObject } from '../../shared/models/display-object';
 import { GenerateDataService } from '../../core/generate-data.service';
 
 @Component({
@@ -11,26 +10,18 @@ import { GenerateDataService } from '../../core/generate-data.service';
   styleUrls: ['./temperature.component.less'],
 })
 export class TemperatureComponent implements OnInit {
-  public isFormed: boolean = false;
-  public displayObject: DisplayObject = {
-    temperature: undefined,
-    humidity: undefined,
-    airPressure: undefined,
-  };
+  public displayObject:
+    | Observable<[string | number, string | number, string | number]>
+    | undefined;
 
   public constructor(private dataService: GenerateDataService) {}
 
   public ngOnInit(): void {
     const maxEventDelay: number = 1000;
-    const minEmitDelay: number = 100;
+    const minPreviousEmitTime: number = 100;
     let temperaturePreviousEventTime: number = Date.now();
     let humidityPreviousEventTime: number = Date.now();
     let airPressurePreviousEventTime: number = Date.now();
-    let previousEmitTime: number = Date.now();
-
-    let lastTimeTemperature: number | string | undefined;
-    let lastTimeHumidity: number | string | undefined;
-    let lastTimeAirPressure: number | string | undefined;
 
     const temperatureEventStream: Observable<Event> = fromEvent(
       document,
@@ -45,85 +36,48 @@ export class TemperatureComponent implements OnInit {
       'generateAirPressure'
     );
 
-    const whetherDataEventsStream: Observable<DisplayObject> = merge(
+    this.displayObject = combineLatest([
       temperatureEventStream.pipe(
-        map(
-          (e: Event): DisplayObject => {
-            const delayFromPreviousEvent: number =
-              Date.now() - temperaturePreviousEventTime;
-            const currentTemperature: number | string =
-              delayFromPreviousEvent > maxEventDelay
-                ? 'N/A'
-                : (e as CustomEvent).detail.temperature;
-            temperaturePreviousEventTime = Date.now();
+        map((e: Event): string | number => {
+          const delayFromPreviousEvent: number =
+            Date.now() - temperaturePreviousEventTime;
+          const temperature: number | string =
+            delayFromPreviousEvent > maxEventDelay
+              ? 'N/A'
+              : (e as CustomEvent).detail.temperature;
+          temperaturePreviousEventTime = Date.now();
 
-            return {
-              temperature: currentTemperature,
-              humidity: lastTimeHumidity,
-              airPressure: lastTimeAirPressure,
-            };
-          }
-        )
+          return temperature;
+        })
       ),
 
       humidityEventStream.pipe(
-        map(
-          (e: Event): DisplayObject => {
-            const delayFromPreviousEvent: number =
-              Date.now() - humidityPreviousEventTime;
-            const currentHumidity: number | string =
-              delayFromPreviousEvent > maxEventDelay
-                ? 'N/A'
-                : (e as CustomEvent).detail.humidity;
-            humidityPreviousEventTime = Date.now();
+        map((e: Event): string | number => {
+          const delayFromPreviousEvent: number =
+            Date.now() - humidityPreviousEventTime;
+          const humidity: number | string =
+            delayFromPreviousEvent > maxEventDelay
+              ? 'N/A'
+              : (e as CustomEvent).detail.humidity;
+          humidityPreviousEventTime = Date.now();
 
-            return {
-              temperature: lastTimeTemperature,
-              humidity: currentHumidity,
-              airPressure: lastTimeAirPressure,
-            };
-          }
-        )
+          return humidity;
+        })
       ),
 
       airPressureEventStream.pipe(
-        map(
-          (e: Event): DisplayObject => {
-            const delayFromPreviousEvent: number =
-              Date.now() - airPressurePreviousEventTime;
-            const currentAirPressure: number | string =
-              delayFromPreviousEvent > maxEventDelay
-                ? 'N/A'
-                : (e as CustomEvent).detail.airPressure;
-            airPressurePreviousEventTime = Date.now();
+        map((e: Event): string | number => {
+          const delayFromPreviousEvent: number =
+            Date.now() - airPressurePreviousEventTime;
+          const airPressure: number | string =
+            delayFromPreviousEvent > maxEventDelay
+              ? 'N/A'
+              : (e as CustomEvent).detail.airPressure;
+          airPressurePreviousEventTime = Date.now();
 
-            return {
-              temperature: lastTimeTemperature,
-              humidity: lastTimeHumidity,
-              airPressure: currentAirPressure,
-            };
-          }
-        )
-      )
-    );
-
-    whetherDataEventsStream.subscribe((data: DisplayObject): void => {
-      const moreThanMinEmitDelay: boolean =
-        Date.now() - previousEmitTime > minEmitDelay;
-      if (moreThanMinEmitDelay) {
-        previousEmitTime = Date.now();
-        lastTimeTemperature = data.temperature;
-        lastTimeHumidity = data.humidity;
-        lastTimeAirPressure = data.airPressure;
-        if (
-          lastTimeTemperature !== undefined &&
-          lastTimeHumidity !== undefined &&
-          lastTimeAirPressure !== undefined
-        ) {
-          this.isFormed = true;
-          this.displayObject = data;
-        }
-      }
-    });
+          return airPressure;
+        })
+      ),
+    ]).pipe(debounceTime(minPreviousEmitTime));
   }
 }
